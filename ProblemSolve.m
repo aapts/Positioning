cac689c2696cf44aa8f554f5ec936c676717106e
arman i/o <armanpts@outlook.com>
2020-03-11 00:46:12 +0300

    plotting, noise addition, solut check function

---
+++ ProblemSolve.m
@@ -0,0 +1,30 @@
+ProblemInit;
+
+plotSpace(beacon, roverInitPosition);
+distNoise = addnoise(distToRover, space, 100);
+
+syms xi phi 
+for i = 1:params.anchorQuantity
+    circle(i) = (beacon(i,1)-xi)^2 + (beacon(i,2)-phi)^2 == distToRover(i)^2;
+    hold on 
+    ezplot(circle(i));
+end
+function plotSpace(beacons,rover)
+    scatter(beacons(:,1),beacons(:,2),'x','magenta');
+    hold on
+    scatter(rover.x,rover.y,...
+          'diamond','black','filled');
+    title('Initial Space')
+    xlabel('X axis');
+    ylabel('Y axis')
+    hold off
+end
+
+function err = calcError(roverInit, roverAcq)
+    err = norm([roverInit.x; roverInit.y] - ...
+               [roverAcq.x;  roverAcq.y]);
+end
+
+function noised = addnoise(dist, space, fineness)
+    noised = dist+ 1 * max(space.x)/numel(space.x) + randn / fineness;
+end
