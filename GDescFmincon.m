ecbb7b1544321619128e1f7485269f320adbde62
arman i/o <armanpts@outlook.com>
2020-03-26 01:53:47 +0300

    Update GDescFmincon.m

--- GDescFmincon.m
+++ GDescFmincon.m
@@ -1,17 +1,20 @@
 % function roverCalcPosition = GDescFmincon(params,beacons,distances)
-% function GDescFmincon
-syms f(x) [1 params.anchorQuantity]
+
+syms f(x,y) [1 params.anchorQuantity]
 
 for i = 1:params.anchorQuantity
     bx = beacons(i,1);
     by = beacons(i,2);
     dst= dTR(i);
-    eval(['f', num2str(i), '(x) = (bx - x(1))^2 + (by - x(2))^2 - dst^2;']);
+    eval(['f', num2str(i), '(x,y) = (bx - x)^2 + (by - y)^2 - dst^2;']);
 end
 f = subs(f);
+eqns(f,1,1)
 
+% fmincon(@(f,x,y)eqns,[0,0], [],[])
 
-% fmincon(@(x) ,[0,0], [],[])
-
+function f = eqns(fun,x,y)
+    f = double(fun(x,y));
+end
 % end
 
