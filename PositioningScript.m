5b8040cc8d6cedf1c54568ca3901d6630327dadd
arman i/o <armanpts@outlook.com>
2020-03-26 15:02:02 +0300

    Update PositioningScript.m

--- PositioningScript.m
+++ PositioningScript.m
@@ -1,5 +1,5 @@
 [params, beacons, dTR, dTRnoised, roverInitPosition, sd] = ...
-          ProblemInit(15, 2, 1500, 10); 
+          ProblemInit(5, 2, 1500, 0); 
 % PlotSpace(beacons,0,roverInitPosition,params) %uncomment to plot the original space
 %% Method 1: an analytical solution, mean of coordinates of intersections of pairs of circles      
 disp('      !!Analytical imtersectioins method')
