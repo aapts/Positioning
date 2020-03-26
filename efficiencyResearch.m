1938b969c0ab1e9b4a33c771d99f353929a0cc63
arman i/o <armanpts@outlook.com>
2020-03-26 15:16:03 +0300

    Update efficiencyResearch.m

--- efficiencyResearch.m
+++ efficiencyResearch.m
@@ -1,1 +1,31 @@
+<<<<<<< Updated upstream
 d
+=======
+[params, beacons, dTR, dTRnoised, roverInitPosition, sd] = ...
+          ProblemInit(5, 2, 1500, 0); 
+      
+%% Method 1: an analytical solution, mean of coordinates of intersections of pairs of circles      
+disp('      !!Analytical imtersectioins method')
+tic
+roverAnalyAcq  = AnalyticalMetod(params,beacons,dTRnoised);
+toc 
+errAnaly = CalcError(roverInitPosition, roverAnalyAcq);
+disp(['distance between acquired and original = ' num2str(errAnaly)])
+
+%% Method 2: Trilaterating the position of the POI
+disp('      !!Trilateration method')
+tic
+roverTrilatAcq  = TrilaterationMethod(params,beacons,dTRnoised);
+toc
+errTrilat = CalcError(roverInitPosition, roverTrilatAcq);
+disp(['distance between acquired and original = ' num2str(errTrilat)])
+
+
+%% Method 3: fmincon. Approaching the solution with the gradient descent
+disp('      !!fmincon method')
+tic
+roverFmincon  = GDescFmincon(params,beacons,dTRnoised);
+toc
+errGDesc = CalcError(roverInitPosition, roverFmincon);
+disp(['distance between acquired and original = ' num2str(errGDesc)])
+>>>>>>> Stashed changes
